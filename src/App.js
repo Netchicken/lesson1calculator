a3ca8f465e7aeaf2709781d50893955ac53e3711
Gary <Gary.Dix@enform.nz>
2022-10-07 11:40:49 +1300

    removed parsefloat

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import logo from "./logo.svg";
+//https://morioh.com/p/2a4e533711d0  https://bobbyhadz.com/blog/react-enter-key-submit-form
 import "./App.css";
 import { useState } from "react";
 
-//https://morioh.com/p/2a4e533711d0  https://bobbyhadz.com/blog/react-enter-key-submit-form
 function App() {
   const [answer, setAnswer] = useState("");
-  const [first, setFirst] = useState("10");
-  const [second, setSecond] = useState("22");
+  const [first, setFirst] = useState(10);
+  const [second, setSecond] = useState(22);
 
   const Calculate = (value) => {
     console.log("value", value);
     if (value !== "") {
       switch (value) {
         case "-":
-          setAnswer(parseFloat(first) - parseFloat(second));
+          setAnswer(first - second);
           break;
         case "+":
-          setAnswer(parseFloat(first) + parseFloat(second));
+          setAnswer(first + second);
           break;
         case "X":
-          setAnswer(parseFloat(first) * parseFloat(second));
+          setAnswer(first * second);
           break;
         case "/":
-          setAnswer(parseFloat(first) / parseFloat(second));
+          setAnswer(first / second);
           break;
 
         default:
@@ -46,13 +45,7 @@ function App() {
         onChange={(event) => setSecond(event.target.value)}
       />
       =
-      <input
-        type='number'
-        id='answer'
-        name='answer'
-        defaultValue={answer}
-        //onChange={(event) => setSecond(event.target.value)}
-      />
+      <input type='number' defaultValue={answer} />
       <div>
         <button className='AppButton' onClick={() => Calculate("+")}>
           +
